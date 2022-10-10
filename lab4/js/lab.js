ac1a7270e5d5b82fcf3e508970085328220999bf
Your Name <mmahelon@ucsc.edu>
2022-10-10 12:51:21 -0700

    Lab 4
    Created lab 4 webpage, formatted and styled with HTML and CSS

---
+++ lab4/js/lab.js
@@ -0,0 +1,18 @@
+// Summary. (use period)
+// Description. (use period)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author Micah Mahelona, Aaron Dangc.
+// @since  x.x.x
+
+//How to save a PDF:
+  //Locate document you want to save
+  //Press CTRL and P to prompt saving
+    //Format destination to “Save as PDF”
+    //Adjust pages, pages per sheet, and margins
+    //Move cursor to bottom of window
+  //Press ”Save”
+
+  //Name file as desired
+    //Select file location to save
+    //Move cursor to bottom of new window
