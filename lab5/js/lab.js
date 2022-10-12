54e342efd5a020c74e696bec798c55af93d4ba79
Your Name <mmahelon@ucsc.edu>
2022-10-12 12:44:31 -0700

    Lab 5
    Lab5 completed!  made variables in .js file.

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/**
+ * Author:    Micah Mahelona
+ * Created:   10.12.2022
+ * Art 101: Computer Programming for the Arts.
+ **/
+
+ //define variables
+ make = "Toyota";
+ model = "Prius Prime";
+ color = "Space Gray";
+ year = "2020";
+
+ //calculate
+ age = 2022 - year;
+
+//print
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + "<br>");
