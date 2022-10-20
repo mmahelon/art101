275d88331bb6f0b5694c6750db538b010aba60d6
Your Name <mmahelon@ucsc.edu>
2022-10-20 13:33:07 -0700

    Lab 7
    Set up basics for Lab 7

---
+++ lab7/js/lab.js
@@ -0,0 +1,24 @@
+/**
+ * Author:    Micah Mahelona, Jorge Gonzalez
+ * Created:   10.19.2022
+ * Art 101: Computer Programming for the Arts.
+ *
+ **/
+function NameSorter(){
+  var userName = window.prompt("Hello! Can you tell me your name so I can organize it?");
+  console.log("userName =", userName);
+  // split string to array
+  var nameArray = userName.split('');
+  console.log("nameArray =", nameArray);
+  // sort array
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort =", nameArraySort);
+  // join array back to a string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted =", nameSorted);
+  return nameSorted;
+}
+
+// output
+document.writeln("What's up? I just finished organizing your name: ",
+  NameSorter(), "</br>");
